128f9bfc35e9b527456d3d323c1243ab3a8ec1f9
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-06-09 22:26:58 +0000

    use logging

--- src/devicedrivers/streamers/ads131_mcast_udp.nim
+++ src/devicedrivers/streamers/ads131_mcast_udp.nim
@@ -296,7 +296,7 @@ proc adcTimerFunc*(timerid: TimerId) {.cdecl.} =
   ## well schucks, that won't work...
   wakeCount.inc()
   if wakeCount mod WAKE_COUNT == 0:
-    timingPrints()
+    logExtraDebug()
   broadcast(adcTimerOpts.timerCond)
 
   logExtraDebug "[adcTimerFunc] timer awake: " & micros().repr()
