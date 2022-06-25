5ebf920308ac93ccaa152a9257151be443aa8ece
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-06-25 04:10:17 +0000

    wrapping more adc things

--- src/devicedrivers/adcs/ads131.nim
+++ src/devicedrivers/adcs/ads131.nim
@@ -1,6 +1,6 @@
 import std/[sequtils, math]
 from os import sleep
-import std/bitops
+import std/[bitops, strutils]
 
 import mcu_utils/basics
 import mcu_utils/basictypes
@@ -71,6 +71,34 @@ type
     FAULT_STATP = 0x12
     FAULT_STATN = 0x13
     GPIO        = 0x14
+
+type
+  ChGain = enum
+    GX1 = 0x00
+    GX2
+    GX4
+    GX6
+    GX8
+    GX12
+  
+  DataRate = enum
+    Dr64k = 0b000
+    Dr32k = 0b001
+    Dr16k = 0b010
+    Dr8k  = 0b011
+    Dr4k  = 0b100
+    Dr2k  = 0b101
+    Dr1k  = 0b111
+  
+  RegConfig1 = distinct uint8
+
+proc `$`(cfg: RegConfig1): string = "RegConfig1(" & cfg.int.toBin(8) & ")"
+proc daisyIn(b: RegConfig1): bool = cast[bool](b.uint8.bitsliced(6..6))
+proc clkEn(b: RegConfig1): bool = cast[bool](b.uint8.bitsliced(5..5))
+proc dataRate(b: RegConfig1): DataRate  = cast[DataRate](b.uint8.bitsliced(0..2))
+proc `daisyIn=`(b: var RegConfig1, x: bool) = b.uint8.setBits(6..6, x)
+proc `clkEn=`(b: var RegConfig1, x: bool) = b.uint8.setBits(5..5, x)
+proc `dataRate=`(b: var RegConfig1, x: DataRate) = b.uint8.setBits(0..2, x)
 
 
 proc spi_debug(self: Ads131Driver) =
--- tests/tads131.nim
+++ tests/tads131.nim
@@ -1,62 +1,35 @@
-import unittest
+import unittest except check
 import strutils
 import bitops
 import print
 
 import mcu_utils/basics
 
-
-type
-  ChGain = enum
-    GX1 = 0x00
-    GX2
-    GX4
-    GX6
-    GX8
-    GX12
-  
-  DataRate = enum
-    Dr64k = 0b000
-    Dr32k = 0b001
-    Dr16k = 0b010
-    Dr8k  = 0b011
-    Dr4k  = 0b100
-    Dr2k  = 0b101
-    Dr1k  = 0b111
-  
-  Config1 = distinct uint8
-
-proc `$`(cfg: Config1): string = "Config1(" & cfg.int.toBin(8) & ")"
-proc daisyIn(b: Config1): bool = cast[bool](b.uint8.bitsliced(6..6))
-proc clkEn(b: Config1): bool = cast[bool](b.uint8.bitsliced(5..5))
-proc dataRate(b: Config1): DataRate  = cast[DataRate](b.uint8.bitsliced(0..2))
-proc `daisyIn=`(b: var Config1, x: bool) = b.uint8.setBits(6..6, x)
-proc `clkEn=`(b: var Config1, x: bool) = b.uint8.setBits(5..5, x)
-proc `dataRate=`(b: var Config1, x: DataRate) = b.uint8.setBits(0..2, x)
+include devicedrivers/adcs/ads131
 
 
 suite "bit ops":
 
   setup:
-    var regCfg1: Config1
+    var regCfg1: RegConfig1
 
   test "dr64k":
     regCfg1.dataRate = Dr64k
     print $regCfg1
     let dr = regCfg1.dataRate
     print dr
-    check dr == Dr64k
+    unittest.check dr == Dr64k
 
   test "dr1k":
     regCfg1.dataRate = Dr1k
     print $regCfg1
     let dr = regCfg1.dataRate
     print dr
-    check dr == Dr1k
+    unittest.check dr == Dr1k
 
   test "dr2k":
     regCfg1.dataRate = Dr2k
     print $regCfg1
     let dr = regCfg1.dataRate
     print dr
-    check dr == Dr2k
+    unittest.check dr == Dr2k
